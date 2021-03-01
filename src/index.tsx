fd452b4d8637142b1db615ececa68875ae402417
deivo <deivo@deivodeiMac.local>
2021-03-01 10:58:02 +0800

    路由的懒加载

--- src/index.tsx
+++ src/index.tsx
@@ -5,29 +5,32 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import store from './store'
 import Tabs from './components/Tabs'
-import Home from './routes/Home'
-import Profile from './routes/Profile'
-import Mine from './routes/Mine'
-import Register from './routes/Register'
-import Login from './routes/Login'
-import Detail from './routes/Detail'
+import { Spin } from 'antd'
+const Home = React.lazy(() => import('./routes/Home'))
+const Profile = React.lazy(() => import('./routes/Profile'))
+const Mine = React.lazy(() => import('./routes/Mine'))
+const Register = React.lazy(() => import('./routes/Register'))
+const Login = React.lazy(() => import('./routes/Login'))
+const Detail = React.lazy(() => import('./routes/Detail'))
 import history from './store/history'
 import './assets/css/common.less'
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <main className="main-container">
-        <Switch>
-          <Route exact={true} path="/" component={Home} />
-          <Route path="/mine" component={Mine} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/detail/:id" component={Detail} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
+      <React.Suspense fallback={<div className="lazy-loading">{<Spin />}</div>}>
+        <main className="main-container">
+          <Switch>
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/mine" component={Mine} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/detail/:id" component={Detail} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </React.Suspense>
       <Tabs />
     </ConnectedRouter>
   </Provider>
